d48ad805f71d8ab7e6b112fba4aa45be38db2fbf
Owinnie <tillawinnie@gmail.com>
2022-07-16 23:56:26 -0700

    header

--- sort.h
+++ sort.h
@@ -27,12 +27,19 @@ typedef enum bool
 	true
 } bool;
 
+/**
+ * enum kind_e - card types
+ * @SPADE: spade
+ * @HEART: heart
+ * @CLUB: club
+ * @DIAMOND: diamond
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -44,8 +51,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -57,9 +64,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void print_list(const listint_t *list);
