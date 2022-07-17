6e4089d1004b885ded154c3bf2bc3486ec8e1d73
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-07-17 11:35:07 +0300

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, postn;
+    int tmp;
+
+    if (array == NULL || size < 2)
+        return;
+    for (i = 0; i < size - 1; i++)
+    {
+        postn = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[postn] > array[j])
+                postn = j;
+        }
+        if (postn != i)
+        {
+            tmp = array[i];
+            array[i] = array[postn];
+            array[postn] = tmp;
+            print_array(array, size);
+        }
+    }
+}
