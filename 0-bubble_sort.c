ad9b0dd6e9e68f69fd0304050a884897d07b7439
Owinnie <tillawinnie@gmail.com>
2022-07-16 23:15:46 -0700

    Bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,26 +6,27 @@
  * @array: array to be sorted
  * @size: size of array
  */
- void bubble_sort(int *array, size_t size)
- {
-     size_t i;
-     int tmp;
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int tmp;
 
-     size_t n = 0;
-     if (array == NULL || size < 2)
-        return;
-     while (n < size)
-     {
-         for (i = 0; i < size - 1; i++)
-         {
-             if (array[i] > array[i + 1])
-             {
-                 tmp = array[i];
-                 array[i] = array[i + 1];
-                 array[i + 1] = tmp;
-                 print_array(array, size);
-             }
-         }
-         n += 1;
-     }
+	size_t n = 0;
+
+	if (array == NULL || size < 2)
+		return;
+	while (n < size)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		n += 1;
+	}
 }
